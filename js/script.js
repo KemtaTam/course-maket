2fbd713e10f9d839970ef75b30845c633316a41c
Максим <41415269+KemtaTam@users.noreply.github.com>
2021-08-02 18:40:04 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+	var mixer = mixitup('.portfolio__content');
+
+	$('.slider-blog__inner').slick({
+		dots: true,
+		
+	/* 	prevArrow: '<button type="button" class="slick-prev"><img src="images/slider-blog/left.png" alt="">Previous</button>',
+		nextArrow: '<button type="button" class="slick-prev"><img src="images/slider-blog/right.png" alt="">Previous</button>', */
+	});
+});
